35caefc36229a5440db0e3b0efd6e6e2e7e84c4f
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-09-14 15:46:14 -0700

    show flash.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action -> {logger.info flash.inspect}
+
   # FIXME: these are just stubs so the routes don't blow up.
   def about;end
   def register;end
