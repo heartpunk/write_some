4e3338c38e4712c682a515fed4eff3287a1155e5
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-10-16 14:35:13 -0700

    fix the worst part of #12.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,12 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-WriteSome::Application.config.secret_key_base = '300ff30c3f2637e5551e8df2cb4684c6fdf1cc1c375227f96770ca38f67889dd66f45294152b31c3c603d7548dbd86d6869df7eb1c49442a792004a17fb6abee'
+not_so_secret_key = '300ff30c3f2637e5551e8df2cb4684c6fdf1cc1c375227f96770ca38f67889dd66f45294152b31c3c603d7548dbd86d6869df7eb1c49442a792004a17fb6abee'
+WriteSome::Application.config.secret_key_base = if ENV['WRITE_SOME_SECRET_KEY'].is_a?(String) and
+                                                   ENV['WRITE_SOME_SECRET_KEY'].length > 30 and
+                                                   ENV['WRITE_SOME_SECRET_KEY'] !~ /#{not_so_secret_key}/
+  ENV['WRITE_SOME_SECRET_KEY']
+else
+  $UNSAFE_SECRET_KEY = true
+  not_so_secret_key
+end
